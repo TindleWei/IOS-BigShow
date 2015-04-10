c256363d69a287ee19484ae0cffc6c18d8ab206d
TindleWei <tindle.wei@gmail.com>
2015-04-10 18:21:17 +0800

    updated BSFindCell

--- BigShow/BSFindCell.h
+++ BigShow/BSFindCell.h
@@ -7,7 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Story.h"
 
 @interface BSFindCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *storyImage;
+@property (weak, nonatomic) IBOutlet UIImageView *characterImage;
+@property (weak, nonatomic) IBOutlet UIView *characterLabel;
+@property (weak, nonatomic) IBOutlet UIView *contentLabel;
+@property (weak, nonatomic) IBOutlet UIView *container;
+
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+
+@property (nonatomic) BOOL canAnimate;
+
+@property (weak, nonatomic) Story *story;
+@property (weak, nonatomic) UITableView *table;
+
+-(void)loadPhoto;
+-(void)stopLoadPhoto;
 
 @end
--- BigShow/BSFindCell.m
+++ BigShow/BSFindCell.m
@@ -7,17 +7,101 @@
 //
 
 #import "BSFindCell.h"
+#import <AVOSCloud/AVOSCloud.h>
+#import "BSProgressView.h"
+
+#define avatarFrame CGRectMake(8, 17, 80, 80)
+
+@implementation UIImageView(Progress)
+
+-(void)setProgressImageWithUrl:(NSString*)url placeholderImage:(UIImage*)placeholderImage{
+    [[self viewWithTag:1000] removeFromSuperview];
+    
+    AVFile* file=[AVFile fileWithURL:url];
+    BOOL hasData = [file isDataAvailable];
+    if(hasData){
+        NSData* data = [file getData];
+        UIImage* image = [UIImage imageWithData:data];
+        self.image = image;
+    } else {
+        self.image=placeholderImage;
+        
+        BSProgressView *pv=[[BSProgressView alloc] initWithWidth:self.frame.size.width/2];
+        pv.bgLineColor=[UIColor colorWithWhite:1 alpha:0.4];
+        pv.fgLineColor=[UIColor whiteColor];
+        pv.tag=1000;
+        [self addSubview:pv];
+        [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            if(error){
+                [pv removeFromSuperview];
+            }else{
+                UIImage* image=[UIImage imageWithData:data];
+                self.image=image;
+                [pv removeFromSuperview];
+            }
+        } progressBlock:^(NSInteger percentDone) {
+            pv.progress=percentDone;
+        }];
+    }
+}
+
+@end
+
+@interface BSFindCell()
+
+@property(weak,nonatomic) NSMutableDictionary *oldFrame;
+
+@end
 
 @implementation BSFindCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(void)loadPhoto {
+    NSArray *pics=[self.story objectForKey:@"pics"];
+    if (pics) {
+        NSString *url=pics[0];
+        url = [url stringByReplacingOccurrencesOfString:@"thumbnail" withString:@"bmiddle"];
+        [self.storyImage setProgressImageWithUrl:url placeholderImage:[UIImage imageNamed:nil]];
+    }
+    
+    int c=pics.count;
+    if (c>1) {
+        self.pageControl.hidden=NO;
+        [self.pageControl setNumberOfPages:pics.count];
+        
+    }else{
+        self.pageControl.hidden=YES;
+    }
+}
 
 - (void)awakeFromNib {
     // Initialization code
 }
 
+-(void)prepareForReuse{
+    [self reset];
+}
+
+
+-(void)reset{
+    if (self.oldFrame) {
+        self.userAvatar.frame=avatarFrame;
+        self.userAvatar.layer.cornerRadius=25;
+        
+        
+        self.oldFrame=nil;
+    }
+}
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
