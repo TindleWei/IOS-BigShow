e27f6bcb40b57109f6ce8899ed232f245dc0469c
TindleWei <tindle.wei@gmail.com>
2015-04-21 17:43:18 +0800

    集成了 图片裁减第三方库PhotoTweaks

--- BigShow/EditFirstC.h
+++ BigShow/EditFirstC.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface EditFirstC : UIViewController<UITextFieldDelegate>
+@interface EditFirstC : UIViewController
 
 @end
--- BigShow/EditFirstC.m
+++ BigShow/EditFirstC.m
@@ -7,11 +7,13 @@
 //
 
 #import "EditFirstC.h"
+#import "PhotoTweaksViewController.h"
 
-@interface EditFirstC()
+@interface EditFirstC() <UITextFieldDelegate, UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate, PhotoTweaksViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *titleText;
 @property (weak, nonatomic) IBOutlet UITextField *nameText;
-@property (weak, nonatomic) IBOutlet UIImageView *avatarText;
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImage;
 
 @end
 
@@ -21,8 +23,31 @@
     [super viewDidLoad];
     self.titleText.delegate = self;
     self.nameText.delegate = self;
+    
+    //Image点击监听
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onClickImage2)];
+    [self.avatarImage addGestureRecognizer:singleTap];
+
 }
 
+-(void)onClickImage2{
+    NSLog(@"图片被点击");
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.delegate = self;
+    picker.allowsEditing = NO;
+    picker.navigationBarHidden = YES;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+//    [UIView animateWithDuration:0.3 animations:^{
+//        self.view.frame = CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
+//    }];
+}
 
 - (BOOL) textFieldShouldReturn:(UITextField *)textField{
     [textField resignFirstResponder];
@@ -32,6 +57,25 @@
         [_nameText resignFirstResponder];
     }
     return YES;
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    PhotoTweaksViewController *photoTweaksViewController = [[PhotoTweaksViewController alloc] initWithImage:image];
+    photoTweaksViewController.delegate = self;
+    photoTweaksViewController.autoSaveToLibray = NO;
+    [picker pushViewController:photoTweaksViewController animated:YES];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)finishWithCroppedImage:(UIImage *)croppedImage{
+    NSLog(@"图片 finishWithCroppedImage");
+    self.avatarImage.image = croppedImage;
 }
 
 @end
