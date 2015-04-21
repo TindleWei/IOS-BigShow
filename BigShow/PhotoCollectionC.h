15329e1dfe46c898af55c8ddf093f5fcc1e4c72c
TindleWei <tindle.wei@gmail.com>
2015-04-21 20:33:33 +0800

    新建了PhotoCollectionC

---
+++ BigShow/PhotoCollectionC.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionC.h
+//  BigShow
+//
+//  Created by tindle on 15/4/21.
+//  Copyright (c) 2015年 tindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCollectionC : UICollectionViewController
+
+@end
---
+++ BigShow/PhotoCollectionC.m
@@ -0,0 +1,23 @@
+//
+//  PhotoCollectionC.m
+//  BigShow
+//
+//  Created by tindle on 15/4/21.
+//  Copyright (c) 2015年 tindle. All rights reserved.
+//
+
+#import "PhotoCollectionC.h"
+
+@interface PhotoCollectionC () <UICollectionViewDataSource,
+    UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+
+@end
+
+@implementation PhotoCollectionC
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
